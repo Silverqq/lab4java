bc173ebd6190976568273060984f82fec72c41ea
Silver <122754864+Silverqq@users.noreply.github.com>
2023-03-03 09:20:10 +0300

    исправил изменение

--- lab4.java
+++ lab4.java
@@ -1,23 +1,26 @@
+package com.company;
+
 import java.util.Scanner;
-public class Main {
-    public static void main(String[]args){
-            Scanner in = new Scanner(System.in);
-            System.out.println("Введите элементы массива: ");
-            int n = in.nextInt();
-            int[] mas = {n};
-            int sum = 0;
-            int min = mas[0];
-            for (int i = 0; i <= mas.length - 1; i++){
-                mas[i]= in.nextInt();
-            }
-            for(int i : mas){
-                sum += i;
-                if(i < min) {
-                    min = i;
-                }
-            }
-            double avg = (double) sum / mas.length;
-            double summa = avg + min;
-            System.out.println(summa);
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите элементы массива: ");
+        int n= in.nextInt();
+        int sum = 0;
+        int[] mas = new int[n];
+        for(int i = 0; i < mas.length; i++){
+            mas[i] = in.nextInt();
         }
+        int min = mas[0];
+        for (int i : mas) {
+            sum += i;
+            if (i < min) {
+                min = i;
+            }
+        }
+        double avg = (double) sum / mas.length;
+        double summa = avg + min;
+        System.out.println(summa);
     }
+}
